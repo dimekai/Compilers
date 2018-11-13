f06581c93b3947bdf5bbc3cdb6dfdb92094aba40
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-11-12 20:16:16 -0600

    HOC completo

--- Practica 05/hoc.h
+++ Practica 05/hoc.h
@@ -7,6 +7,7 @@ typedef struct Symbol{    /*Entrada de la tabla de símbolos*/
 
     union{
       Vector * val;       /* Si es VAR */
+      Vector *(*ptr)();   /* Si es BLTIN */
       double num;
     }u;
 
@@ -16,9 +17,10 @@ typedef struct Symbol{    /*Entrada de la tabla de símbolos*/
 Symbol * install(char * s, int t, Vector * d);
 Symbol * lookup(char * s);
 
-typedef union Datum{
+typedef union Datum{    /* Tipo de pila del interprete */
   Vector * val;
   double num;
+  int eval;
   Symbol * sym;   /*Apunta a una entrada en la tabla de simbolos */
 }Datum;
 
@@ -28,6 +30,8 @@ typedef int (* Inst)(); /* Instruccion de maquina:
 
 #define STOP (Inst) 0
 extern Inst prog[];
+extern Inst * progp;
+extern Inst * code();
 
 extern void assign();
 extern void varpush();
@@ -35,7 +39,9 @@ extern void constpush();
 extern void print();
 extern void printd();
 extern void constpushd();
+extern void bltin();
 
+/* Funciones con los vectores */
 extern void eval();
 extern void add();
 extern void sub();
@@ -43,3 +49,19 @@ extern void producto_cruz();
 extern void producto_punto(); 
 extern void magnitud();
 extern void escalar();
+
+/* Operadores condicionales aritméticos */
+extern void gt();
+extern void lt();
+extern void ge();
+extern void le();
+extern void eq();
+extern void ne();
+extern void and();
+extern void or();
+extern void not();
+
+/* Condicion y Ciclo */
+extern void whilecode();
+extern void ifcode();
+extern void prexpr();
